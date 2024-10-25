1aac1dcff53cbef0225a84679c8964fedc81a352
Josué Brenes <josuebrenes3005@gmail.com>
2024-10-24 20:37:18 -0600

    Changes in Offers

--- Frontend/Application/src/components/escrow/offers/offers.tsx
+++ Frontend/Application/src/components/escrow/offers/offers.tsx
@@ -1,88 +1,47 @@
-import React from 'react'
-import Header from "@/layouts/Header";
+import React from "react";
 import { SlLike } from "react-icons/sl";
 import { SlDislike } from "react-icons/sl";
 import { FaUser } from "react-icons/fa";
 
 const Offers: React.FC = () => {
-    return (
-        <div>
-            <Header />
-        
-        <div className="bg-white min-h-screen text-black py-6 px-4">
-            
-            <div className="max-w-full mx-auto mb-4">
-                <h1 className='font-bold text-2xl mb-2'>Puerto Viejo House · <span className="text-gray-500">Interested People</span></h1>
-                <div>
-                    <p className="text-gray-700">1,310 km away · 2 bedrooms · 2 beds · 1 bathroom</p>
-                </div>
-            </div>
-            <div className="overflow-x-auto">
-                <table className="min-w-full bg-white border border-gray-200">
-                    <thead>
-                        <tr className="bg-gray-200 text-gray-600 uppercase text-sm">
-                            <th className="py-3 px-4 text-left">Wallet</th>
-                            <th className="py-3 px-4 text-left">Reservation Date</th>
-                            <th className="py-3 px-4 text-left">Actions</th>
-                        </tr>
-                    </thead>
-                    <tbody className="text-gray-700">
-                        <tr className="border-b hover:bg-gray-100">
-                            <td className="py-3 px-4 flex items-center">
-                                <FaUser className="mr-2" /> QXSZ...23S
-                            </td>
-                            <td className="py-3 px-4">2024-10-25</td>
-                            <td className="py-3 px-4">
-                                <button className="bg-green-500 text-white text-lg p-3 me-1 rounded-lg hover:underline"><SlLike /></button>
-                                <button className="bg-red-500 text-white text-lg p-3 ms-1 rounded-lg hover:underline"><SlDislike /></button>
-                            </td>
-                        </tr>
-                        <tr className="border-b hover:bg-gray-100">
-                            <td className="py-3 px-4 flex items-center">
-                                <FaUser className="mr-2" /> P2OZ...378
-                            </td>
-                            <td className="py-3 px-4">2024-10-25</td>
-                            <td className="py-3 px-4">
-                                <button className="bg-green-500 text-white text-lg p-3 me-1 rounded-lg hover:underline"><SlLike /></button>
-                                <button className="bg-red-500 text-white text-lg p-3 ms-1 rounded-lg hover:underline"><SlDislike /></button>
-                            </td>
-                        </tr>
-                        <tr className="border-b hover:bg-gray-100">
-                            <td className="py-3 px-4 flex items-center">
-                                <FaUser className="mr-2" /> QXSZ...23S
-                            </td>
-                            <td className="py-3 px-4">2024-10-25</td>
-                            <td className="py-3 px-4">
-                                <button className="bg-green-500 text-white text-lg p-3 me-1 rounded-lg hover:underline"><SlLike /></button>
-                                <button className="bg-red-500 text-white text-lg p-3 ms-1 rounded-lg hover:underline"><SlDislike /></button>
-                            </td>
-                        </tr>
-                        <tr className="border-b hover:bg-gray-100">
-                            <td className="py-3 px-4 flex items-center">
-                                <FaUser className="mr-2" /> QXSZ...23S
-                            </td>
-                            <td className="py-3 px-4">2024-10-25</td>
-                            <td className="py-3 px-4">
-                                <button className="bg-green-500 text-white text-lg p-3 me-1 rounded-lg hover:underline"><SlLike /></button>
-                                <button className="bg-red-500 text-white text-lg p-3 ms-1 rounded-lg hover:underline"><SlDislike /></button>
-                            </td>
-                        </tr>
-                        <tr className="border-b hover:bg-gray-100">
-                            <td className="py-3 px-4 flex items-center">
-                                <FaUser className="mr-2" /> QXSZ...23S
-                            </td>
-                            <td className="py-3 px-4">2024-10-25</td>
-                            <td className="py-3 px-4">
-                                <button className="bg-green-500 text-white text-lg p-3 me-1 rounded-lg hover:underline"><SlLike /></button>
-                                <button className="bg-red-500 text-white text-lg p-3 ms-1 rounded-lg hover:underline"><SlDislike /></button>
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
-        </div>
-        </div>
-    )
-}
+  return (
+    <div className="max-w-4xl w-full  p-6 rounded-lg ">
+      <h1 className="text-3xl text-black font-semibold mb-2">
+        Puerto Viejo House
+      </h1>
+      <p className="text-black text-lg mb-4">
+        A 130 km de distancia · 2 habitaciones · 2 camas · 1 baño
+      </p>
+
+      <div className="overflow-x-auto">
+        <table className="min-w-full bg-white border border-gray-300">
+          <thead>
+            <tr className="bg-gray-200 text-gray-600 uppercase text-sm">
+              <th className="py-3 px-6 text-left">Wallet</th>
+              <th className="py-3 px-6 text-left">Reservation Date</th>
+              <th className="py-3 px-6 text-left">Actions</th>
+            </tr>
+          </thead>
+          <tbody className="text-gray-700 text-sm">
+            <tr className="border-b hover:bg-gray-100">
+              <td className="py-3 px-6 flex items-center">
+                <FaUser className="mr-2" /> QXSZ...23S
+              </td>
+              <td className="py-3 px-6">2024-10-25</td>
+              <td className="py-3 px-6 flex space-x-2">
+                <button className="bg-green-500 text-white p-2 rounded-lg hover:bg-green-600">
+                  <SlLike />
+                </button>
+                <button className="bg-red-500 text-white p-2 rounded-lg hover:bg-red-600">
+                  <SlDislike />
+                </button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
 
 export default Offers;
