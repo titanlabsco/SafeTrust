2b5e795f9859806e53a1d24822bb7e46d36044b3
Josué Brenes <josuebrenes3005@gmail.com>
2024-11-09 01:57:57 -0600

    styles: format code

--- src/components/home/Filters.tsx
+++ src/components/home/Filters.tsx
@@ -31,10 +31,7 @@ const Filters: React.FC = () => {
   const [activeFilter, setActiveFilter] = useState<string>("Tropical");
 
   return (
-    <div 
-    
-    
-    className="flex justify-center space-x-8 py-4 bg-white shadow-sm">
+    <div className="flex justify-center space-x-8 py-4 bg-white shadow-sm">
       {filters.map((filter) => (
         <div
           key={filter.label}
