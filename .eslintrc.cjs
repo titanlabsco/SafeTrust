974f95bb8a25b3cf409c9e73c2636b7baac81322
Josué Brenes <josuebrenes3005@gmail.com>
2024-11-09 01:56:05 -0600

    ci: add eslint configuration

---
+++ .eslintrc.cjs
@@ -0,0 +1,17 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:prettier/recommended",
+  ],
+  ignorePatterns: ["dist", ".next", "node_modules", ".eslintrc.cjs"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["prettier"],
+  rules: {
+    "prettier/prettier": "error",
+    "@typescript-eslint/no-explicit-any": "warn",
+  },
+};
