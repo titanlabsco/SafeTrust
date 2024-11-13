87ceaaa9cbb60dbe0bc9eeba9b461809d4999316
Josué Brenes <josuebrenes3005@gmail.com>
2024-11-12 19:51:07 -0600

    feat: add new login

--- src/components/auth/login/AuthButtons.tsx
+++ src/components/auth/login/AuthButtons.tsx
@@ -1,10 +1,22 @@
-import { FaGithub } from "react-icons/fa";
+import { FcGoogle } from 'react-icons/fc';
+import { FaWallet } from 'react-icons/fa'; 
 
 const AuthButtons = () => {
   return (
-    <div className="flex flex-col space-y-3">
-      <button className="flex items-center justify-center w-full bg-gray-800 text-white py-3 rounded-md hover:bg-gray-700 transition text-base">
-        <FaGithub className="mr-2" /> Sign up with GitHub
+    <div className="space-y-2">
+      <button
+        type="button"
+        className="w-full py-2 px-4 border text-black border-gray-300 rounded-md flex items-center justify-center hover:bg-gray-100"
+      >
+        <FcGoogle className="w-5 h-5 mr-2" />
+        Login with Google
+      </button>
+      <button
+        type="button"
+        className="w-full py-2 px-4 bg-black text-white rounded-md flex items-center justify-center hover:bg-gray-800"
+      >
+        <FaWallet className="w-5 h-5 mr-2" />
+        Login with Wallet
       </button>
     </div>
   );
