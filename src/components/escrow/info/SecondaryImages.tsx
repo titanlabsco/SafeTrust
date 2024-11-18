c393c779651adb2322d4f3bedb397efbacd88bd1
Josué Brenes <josuebrenes3005@gmail.com>
2024-11-18 00:15:37 -0600

    feat: change url image

--- src/components/escrow/info/SecondaryImages.tsx
+++ src/components/escrow/info/SecondaryImages.tsx
@@ -5,14 +5,14 @@ const SecondaryImages: React.FC = () => {
   return (
     <div className="grid grid-rows-2 gap-2 h-[20rem]">
       <Image
-        src="/img/house2.jpg"
+        src="/img/house1.jpg"
         alt="Secondary"
         width={400}
         height={250}
         className="rounded-lg w-full h-[10rem] object-cover"
       />
       <Image
-        src="/img/house3.jpg"
+        src="/img/house1.jpg"
         alt="Secondary"
         width={400}
         height={250}
