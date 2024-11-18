2ed6a3a67c4c6900a593895d81aaf04fabc973a6
Josué Brenes <josuebrenes3005@gmail.com>
2024-11-18 16:01:22 -0600

    feat: add pending page

---
+++ src/layouts/house/pending/Header.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Header: React.FC = () => {
+  return (
+    <div className="flex justify-between items-center mb-6">
+      <h1 className="text-2xl font-bold text-black flex items-center gap-2">
+        INV4257-09-012
+        <span className="bg-red-200 text-red-600 text-sm font-semibold px-4 py-1 rounded-full">
+          Pending
+        </span>
+      </h1>
+    </div>
+  );
+};
+
+export default Header;
