aaade87a3fb99c06dda591d095c1ca894729f5d1
DiegoB1911 <diego.barquero@hotmail.com>
2024-11-20 11:10:55 -0600

    deleting comment

--- src/components/map/index.tsx
+++ src/components/map/index.tsx
@@ -1,5 +1,5 @@
 "use client"
-import dynamic from "next/dynamic"; // Importa dynamic
+import dynamic from "next/dynamic";
 
 const MapComponent = dynamic(() => import("@/components/map/Map"), { ssr: false });
 
