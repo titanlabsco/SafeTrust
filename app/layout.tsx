04ff169ba50a3e7d9f5afab3d571070b16461b60
DiegoB1911 <diego.barquero@hotmail.com>
2024-11-21 08:27:48 -0600

    fix: removing authentication

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,11 +10,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className="antialiased font-inter">
-        <ClientProviders>
           <div className="min-h-screen flex flex-col">
             <main className="flex-1">{children}</main>
           </div>
-        </ClientProviders>
       </body>
     </html>
   );
