c49592e3d3f4bf9a112a3605972908d72a932574
QuiqueCode <dylanbusisness@gmail.com>
2024-11-22 15:02:30 -0600

    feat: add i18next config

---
+++ src/i18n/config.ts
@@ -0,0 +1,15 @@
+import i18n from 'i18next'
+import {initReactI18next} from 'react-i18next'
+import resources from './resources'
+
+const language=localStorage.getItem('LngPreference') || (navigator.language ? navigator.language.split('-')[0]:"en");
+
+i18n.use(initReactI18next).init({
+    resources,
+    lng:language,
+    fallbackLng:'en',
+    defaultNS:"translations",
+    interpolation:{
+        escapeValue:false
+    }
+});
