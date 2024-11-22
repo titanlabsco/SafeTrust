ec610216cfc7ab7cbe424442fe90b938b44bd9d7
KevinLatino <latinokevin9@gmail.com>
2024-11-21 18:23:21 -0600

    Implement My Apartments View issue #78

--- src/components/map/Map.tsx
+++ src/components/map/Map.tsx
@@ -1,22 +1,22 @@
-import React from 'react';
-import { MapContainer, TileLayer } from 'react-leaflet';
-import { LatLngTuple } from 'leaflet';
+import React from "react";
+import { MapContainer, TileLayer } from "react-leaflet";
+import { LatLngTuple } from "leaflet";
 
 const MapComponent: React.FC = () => {
-    const center: LatLngTuple = [37.7749, -122.4194];
+  const center: LatLngTuple = [37.7749, -122.4194];
 
-    return (
-        <MapContainer
-        center={center}
-        zoom={13}
-        style={{ height: '400px', width: '100%' }}
-        >
-            <TileLayer
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                attribution="&copy; OpenStreetMap contributors"
-            />
-        </MapContainer>
-    );
+  return (
+    <MapContainer
+      center={center}
+      zoom={13}
+      style={{ height: "400px", width: "100%" }}
+    >
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution="&copy; OpenStreetMap contributors"
+      />
+    </MapContainer>
+  );
 };
 
 export default MapComponent;
