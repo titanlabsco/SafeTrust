5431876265055c7c5660dfdde00d60b19e99462d
villarley <115122095+Villarley@users.noreply.github.com>
2024-11-23 21:30:52 -0600

    Feat: Verification code input added.
    - Implemented the component for the verification code input
    - Minimal update to the city background comp

--- src/features/auth/components/CityBackground/index.tsx
+++ src/features/auth/components/CityBackground/index.tsx
@@ -7,12 +7,11 @@ interface Props {
 export const CityBackground: React.FC<Props> = ({ className }) => {
   return (
     <div
-      className={⁠ absolute bottom-0 w-full bg-no-repeat bg-contain ${className} ⁠}
+      className={`absolute bottom-0 w-full h-[500px] bg-no-repeat bg-contain ${className}`}
       style={{
         backgroundImage: "url('/img/apartments.png')",
         backgroundPosition: "center",
         backgroundSize: "cover",
-        height: "80vh",
       }}
     ></div>
   );
