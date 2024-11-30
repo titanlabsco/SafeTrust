6d767551f4c4d1311c76e76c825efabe0f36dc29
QuiqueCode <dylanbusisness@gmail.com>
2024-11-30 13:46:18 -0600

    feat: add translations for my apartments section

--- app/profile/my-apartments/page.tsx
+++ app/profile/my-apartments/page.tsx
@@ -2,18 +2,20 @@
 import Header from "@/layouts/Header";
 import { IoHomeOutline } from "react-icons/io5";
 import ApartmentsComponent from "@/components/profile/pages/ApartmentsComponent";
+import { useTranslation } from "react-i18next";
 
 const MyApartments: React.FC = () => {
+  const {t}=useTranslation();
   return (
     <div className="bg-gray-50 min-h-screen flex flex-col">
       <Header />
       <section className="flex flex-col sm:flex-row sm:justify-between items-start sm:items-center w-full pt-16 px-6 sm:px-24 gap-4">
         <h1 className="text-[#000000] font-bold text-[28px] sm:text-[36px]">
-          My apartments
+          {t("myApartments.title")}
         </h1>
-        <button className="bg-custom-orange w-full sm:w-[157px] font-bold text-[16px] text-white h-[36px] rounded-[4px] flex justify-center items-center gap-2">
+        <button className="bg-custom-orange w-full sm:w-[210px] font-bold text-[16px] text-white h-[36px] rounded-[4px] flex justify-center items-center gap-2">
           <IoHomeOutline className="w-[20px] h-[20px]" />
-          New apartment
+          {t("myApartments.newApartmentBtn")}
         </button>
       </section>
 
