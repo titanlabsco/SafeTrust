081d8dd46d5df232b6b166ade9d4b927bae547eb
MPSxDev <mparedes.digital@gmail.com>
2024-12-14 00:40:11 -0300

    feat: Add darkmode to the house/paid components

--- src/components/house/paid/Details.tsx
+++ src/components/house/paid/Details.tsx
@@ -2,11 +2,11 @@ import React from 'react';
 
 const Details: React.FC = () => {
   return (
-    <div className="col-span-8 bg-white rounded-lg shadow-lg p-6">
-      <h2 className="text-xl font-bold text-black mb-4">
+    <div className="col-span-8 bg-light-secondary dark:bg-dark-surface rounded-lg shadow-lg p-6">
+      <h2 className="text-xl font-bold text-light-primary dark:text-dark-primary mb-4">
         Payment batch January 2025
       </h2>
-      <div className="grid grid-cols-2 gap-4 text-sm text-gray-700">
+      <div className="grid grid-cols-2 gap-4 text-sm text-light-primary dark:text-dark-primary">
         <div>
           <p>
             <strong>Bill to:</strong> john_st@gmail.com
@@ -35,23 +35,23 @@ const Details: React.FC = () => {
       </div>
       <hr className="my-6 border-gray-300" />
       <div className="mb-6">
-        <table className="w-full text-sm border-t border-b border-gray-300">
+        <table className="w-full text-sm border-t border-b border-light-primary dark:border-dark-primary">
           <thead>
-            <tr className="bg-gray-100">
-              <th className="text-left font-semibold py-2 px-4 text-black">
+            <tr className="bg-light-secondary dark:bg-dark-surface">
+              <th className="text-left font-semibold py-2 px-4 text-light-primary dark:text-dark-primary">
                 PRODUCT
               </th>
-              <th className="text-left font-semibold py-2 px-4 text-black">
+              <th className="text-left font-semibold py-2 px-4 text-light-primary dark:text-dark-primary">
                 PRICE PER MONTH
               </th>
-              <th className="text-left font-semibold py-2 px-4 text-black">
+              <th className="text-left font-semibold py-2 px-4 text-light-primary dark:text-dark-primary">
                 DEPOSIT
               </th>
             </tr>
           </thead>
           <tbody>
             <tr>
-              <td className="py-2 px-4 flex items-center text-black">
+              <td className="py-2 px-4 flex items-center text-light-primary dark:text-dark-primary">
                 <img
                   src="/img/house1.jpg"
                   alt="La sabana apartment"
@@ -59,26 +59,26 @@ const Details: React.FC = () => {
                 />
                 <span>La sabana apartment</span>
               </td>
-              <td className="py-2 px-4 text-black">$4,000</td>
-              <td className="py-2 px-4 text-black">$4,000</td>
+              <td className="py-2 px-4 text-light-primary dark:text-dark-primary">$4,000</td>
+              <td className="py-2 px-4 text-light-primary dark:text-dark-primary">$4,000</td>
             </tr>
           </tbody>
         </table>
       </div>
-      <div className="text-sm text-black flex flex-col items-start mt-2">
+      <div className="text-sm text-light-primary dark:text-dark-primary flex flex-col items-start mt-2">
         <div className="flex justify-between w-full mb-0.5">
-          <p className="font-bold text-black mr-2">Subtotal:</p>
+          <p className="font-bold text-light-primary dark:text-dark-primary mr-2">Subtotal:</p>
           <p>$4,000</p>
-          <p className="font-bold text-black ml-2">Total:</p>
+          <p className="font-bold text-light-primary dark:text-dark-primary ml-2">Total:</p>
           <p>$8,000</p>
         </div>
         <div className="flex justify-between w-full mt-0.5">
-          <p className="font-bold text-black">Discount (10%)</p>
+          <p className="font-bold text-light-primary dark:text-dark-primary">Discount (10%)</p>
         </div>
       </div>
       <div className="mt-6">
-        <h3 className="text-lg font-bold text-black">Terms & Conditions</h3>
-        <p className="text-gray-500 text-sm mt-2">
+        <h3 className="text-lg font-bold text-light-primary dark:text-dark-primary">Terms & Conditions</h3>
+        <p className="text-light-primary dark:text-dark-primary text-sm mt-2">
           Lorem Ipsum is simply dummy text of the printing and typesetting
           industry. Lorem Ipsum has been the industry's standard dummy text ever
           since the 1500s, when an unknown printer took a galley of type and
